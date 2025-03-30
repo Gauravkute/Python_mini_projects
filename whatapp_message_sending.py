df024cfbb033d6fda2dd2b1b0266cc79a0dd928e
Gaurav Kute <kuteg2915@gmail.com>
2025-03-30 10:10:10 +0530

    Add files via upload

---
+++ whatapp_message_sending.py
@@ -0,0 +1,5 @@
+#pip install pywhatkit
+import pywhatkit as pyw
+
+pyw.sendwhatmsg('','Automatic message using python',10,4) #phone number with country code,Message,time in hour,time in second
+#laptop / pc should be on at that time a file will also created a text file 
